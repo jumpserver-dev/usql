b8a3d23b361edbf0dbec20543f4d12b80b49c263
Eric <xplzv@126.com>
2024-12-17 13:00:49 +0800

    perf: mysql support ipv6 address

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"context"
+	"log"
+	"os"
+
 	_ "github.com/jumpserver-dev/usql/drivers/mysql"
 	_ "github.com/xo/usql/drivers/clickhouse"
 	_ "github.com/xo/usql/drivers/oracle"
 	_ "github.com/xo/usql/drivers/postgres"
 	_ "github.com/xo/usql/drivers/sqlserver"
-	"log"
-	"os"
 )
 
 func main() {
